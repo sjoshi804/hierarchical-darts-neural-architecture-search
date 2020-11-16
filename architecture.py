cd8623c34fb3d8fe25261eb4c7da8cc168eedc54
Siddharth Joshi <sjoshi804@gmail.com>
2020-11-16 14:28:41 +0530

    generalized create graph by making recursive - NOT TESTED

--- architecture.py
+++ architecture.py
@@ -1,28 +1,31 @@
 from edge import Edge
-
+from copy import deepcopy 
 class Architecture:
     def __init__(self, num_levels, num_nodes_at_level, base_operations):
+
+        # Set member variables
         self.num_levels = num_levels
         self.num_nodes_at_level = num_nodes_at_level
         self.base_operation = base_operations
-        self.initialize()
 
-    def initialize(self): #TODO: Must be generalized for any number of levels - JUST MAKE IT RECURSIVE
-        # Loop through all possible edges for dag at level 2
-        for level_2_node_a in range(self.num_nodes_at_level[2]):
-            for level_2_node_b in range(level_2_node_a + 1, self.num_nodes_at_level[2]):
-                level_2_sub_dag = {} # Initialize empty dict to contain all edges for the sub dag
-                # Loop through all possible edges for dag at level 1
-                for level_1_node_a in range(self.num_nodes_at_level[1]):
-                    for level_1_node_b in range(level_1_node_a + 1, self.num_nodes_at_level[1]):
-                        level_1_sub_dag = {} # Initialize empty dict to contain all edges for the sub dag
-                        #Loop through all possible edges for dag at level 0
-                        for level_0_node_a in range(self.num_nodes_at_level[0]):
-                            for level_0_node_b in range(level_0_node_a + 1, self.num_nodes_at_level[0]):
-                                level_1_sub_dag[(level_0_node_a, level_0_node_b)] = Edge((1), None, 0) # Add this edge to the sub_dag for level above
-                        level_2_dag[(level_1_node_a, level_1_node_b)] = Edge((1), level_1_sub_dag, 1) # Add this edge to the sub_dag for level above
-                architecure[(level_2_node_a, level_2_node_b)] = Edge((1), level_2_sub_dag, 2) # Add this edge to the sub_dag for level above
-    
+        # Create computational graph
+        self.computational_graph = self.create_graph()
+
+    def create_graph(self, level):
+        dag = {}
+
+        # Generate sub_dag once and use deepcopies of it across the level
+        if level == 0:
+            sub_dag = None
+        else:
+            sub_dag = create_graph(level - 1)
+
+        for node_a in range(self.num_nodes_at_level[level]):
+            for node_b in range(node_a + 1, self.num_nodes_at_level[level]):
+                dag[(node_a, node_b)] = Edge((1), deepcopy(sub_dag), level)
+
+        return dag
+        
     #TODO:
     def get_alpha_i(self, level):
         raise NotImplementedError()
