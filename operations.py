0b05005bb5dd5cd4f3e5cb5903855f602a0f7c74
Siddharth Joshi <sjoshi804@gmail.com>
2020-12-17 13:13:34 +0530

    Bug Fix: conv7x1_1x7 fixed to have channels_out

--- operations.py
+++ operations.py
@@ -99,17 +99,25 @@ OPS = {
   'sep_conv_7x7' : lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
   'dil_conv_3x3' : lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
   'dil_conv_5x5' : lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
-  
-  'conv_7x1_1x7' : lambda C, stride, affine: nn.Sequential(
-   nn.ReLU(inplace=False),
-   nn.Conv2d(C, C, (1,7), stride=(1, stride), padding=(0, 3), bias=False),
-   nn.Conv2d(C, C, (7,1), stride=(stride, 1), padding=(3, 0), bias=False),
-   nn.BatchNorm2d(C, affine=affine)
-   ),
+  'conv_7x1_1x7' : lambda C, stride, affine: Conv7x1_1x7(C, stride, affine=affine)
 }
 
 LEN_OPS = len(OPS)
 
+class Conv7x1_1x7(nn.Module):
+  def __init__(self, C, stride, affine):
+    super().__init__()
+    self.op = nn.Sequential(
+                            nn.ReLU(inplace=False),
+                            nn.Conv2d(C, C, (1,7), stride=(1, stride), padding=(0, 3), bias=False),
+                            nn.Conv2d(C, C, (7,1), stride=(stride, 1), padding=(3, 0), bias=False),
+                            nn.BatchNorm2d(C, affine=affine)
+                            )
+    self.channels_out = C  
+  
+  def forward(self, x):
+    return self.op(x)
+    
 
 class AvgPool2d(nn.Module): 
   def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True, count_include_pad=False):
