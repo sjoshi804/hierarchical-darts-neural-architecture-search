0ed6ce9265e0f45bafdc91ec5bb5273906f0f922
Siddharth Joshi <sjoshi804@gmail.com>
2021-04-14 23:08:03 -0700

    Bug Fix: Disconnect input / output on base level as well by mistake

--- hierarchical_operation.py
+++ hierarchical_operation.py
@@ -176,7 +176,7 @@ class HierarchicalOperation(nn.Module):
         
         # If input node at top level, then do not connect to output node
         # If input node at top level, do not connect to other input node
-        if (node_a < 2) and ((node_b == 1) or (node_b == num_nodes - 1)):
+        if (level == alpha.num_levels - 1) and(node_a < 2) and ((node_b == 1) or (node_b == num_nodes - 1)):
           continue 
 
         # Create mixed operation on outgiong edge
